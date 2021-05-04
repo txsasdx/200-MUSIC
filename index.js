3f450999f6d5af1b538a5a591630d20223240b0e
txsasdx <83542598+txsasdx@users.noreply.github.com>
2021-05-04 19:38:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
-require("dotenv").config();
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const fs = require("fs");
-const Enmap = require("enmap");
-
-client.config = {
-  token: process.env.DISCORD_TOKEN,
-  prefix: process.env.DISCORD_PREFIX,
-  api: process.env.GOOGLE_API,
-};
-client.commands = new Enmap();
-client.queue = new Map();
-
-client.once("ready", () =>
-  console.log("Ready, Logged in as " + client.user.tag)
-);
-
-fs.readdir(__dirname + "/commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    client.commands.set(commandName, props);
-    console.log("Loading Command: "+commandName)
-  });
+CONST Bot = require('ctk-musicbot');
+ 
+CONST musicbot = new Bot({
+  Token: 'ODM4NDMzMjIwNzg1MDc4MzMy.YI7ByQ.MgMczv_r1ixML6ic37LskwQYOAc',
+  prefix: '!'
 });
-
-client.on("message", (message) => {
-  if (!message.content.startsWith(client.config.prefix) || message.author.bot)
-    return;
-  const args = message.content.slice(client.config.prefix.length).split(/ +/);
-  const commandName = args.shift().toLowerCase();
-  const command = client.commands.get(commandName);
-  if (!command) return;
-
-  command.run(client, message, args);
-});
-
-client.login(client.config.token);
+ 
+musicbot.start();
